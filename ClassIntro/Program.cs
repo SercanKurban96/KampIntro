862767aa9f95ff7f305d067d9aa663fb148307ea
user <user@DESKTOP-9H60TTH>
2022-10-17 00:32:05 +0300

    Add project files.

---
+++ ClassIntro/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassIntro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string adi = "Sercan";
+            int yas = 26;
+
+            Kurs kurs1 = new Kurs();
+            kurs1.kursAdi = "C#";
+            kurs1.Egitmen = "Sercan KURBAN";
+            kurs1.IzlenmeOrani = 68;
+
+            Kurs kurs2 = new Kurs();
+            kurs2.kursAdi = "Java";
+            kurs2.Egitmen = "Ahmet KARACA";
+            kurs2.IzlenmeOrani = 64;
+
+            Kurs kurs3 = new Kurs();
+            kurs3.kursAdi = "Python";
+            kurs3.Egitmen = "Yusuf KANDEMİR";
+            kurs3.IzlenmeOrani = 80;
+
+            Kurs kurs4 = new Kurs();
+            kurs4.kursAdi = "C++";
+            kurs4.Egitmen = "Yeliz ASYA";
+            kurs4.IzlenmeOrani = 100;
+
+            //Console.WriteLine(kurs1.kursAdi + " : " + kurs1.Egitmen);
+
+            Kurs[] kurslar = new Kurs[] { kurs1, kurs2, kurs3, kurs4 };
+
+            foreach (var kurs in kurslar)
+            {
+                Console.WriteLine(kurs.kursAdi + " : " + kurs.Egitmen);
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+
+    class Kurs
+    {
+        // prop -> TAB+TAB
+        public string kursAdi { get; set; }
+        public string Egitmen { get; set; }
+        public int IzlenmeOrani { get; set; }
+    }
+}
---
+++ Koleksiyonlar/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Koleksiyonlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] { "Sercan", "Ahmet", "Yusuf", "Yeliz" };
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+            //Console.WriteLine(isimler[2]);
+            //Console.WriteLine(isimler[3]);
+
+            //isimler = new string[5];
+            //isimler[4] = "Leyla";
+            //Console.WriteLine(isimler[4]);
+            //Console.WriteLine(isimler[0]);
+
+            List<string> isimler2 = new List<string> { "Sercan", "Ahmet", "Yusuf", "Yeliz" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[3]);
+            isimler2.Add("Leyla");
+            Console.WriteLine(isimler2[4]);
+            Console.WriteLine(isimler2[0]);
+
+            Console.ReadLine(); 
+        }
+    }
+}
